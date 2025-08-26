cf7560300b9717966472de27ef719869b8ff1cb9
gustavorizeriodev <gustavogrizeriog@gmail.com>
2025-08-26 19:36:25 -0300

    feat: adicionando projeto

---
+++ src/main/java/com/arquitetura_limpa/Atividade/AtividadeArquiteturaLimpaApplication.java
@@ -0,0 +1,13 @@
+package com.arquitetura_limpa.Atividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeArquiteturaLimpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeArquiteturaLimpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquitetura_limpa/Atividade/AtividadeArquiteturaLimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitetura_limpa.Atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeArquiteturaLimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
