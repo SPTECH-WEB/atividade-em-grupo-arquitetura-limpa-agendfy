e768ea43fbe894f927c06e1f4e84d6261e6d42ef
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2025-08-26 20:41:18 -0300

    criação de classe de email

---
+++ src/main/java/com/arquitetura_limpa/Atividade/core/application/RegraClassificacaoUseCase.java
@@ -0,0 +1,4 @@
+package com.arquitetura_limpa.Atividade.core.application;
+
+public class RegraClassificacaoUseCase {
+}
---
+++ src/main/java/com/arquitetura_limpa/Atividade/core/domain/ValueObjects/Email.java
@@ -0,0 +1,4 @@
+package com.arquitetura_limpa.Atividade.core.domain.ValueObjects;
+
+public class Email {
+}
