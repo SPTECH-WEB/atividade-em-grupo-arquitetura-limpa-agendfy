cddf20b0be93bf0032d86403d2d0a9af32422f7a
KauanPaschoal <kauan.psantos@sptech.school>
2025-08-26 20:40:18 -0300

    feat: adicionando atributos de cliente e valueObjects idade

--- src/main/java/com/arquitetura_limpa/Atividade/core/domain/Cliente.java
+++ src/main/java/com/arquitetura_limpa/Atividade/core/domain/Cliente.java
@@ -1,5 +1,28 @@
 package com.arquitetura_limpa.Atividade.core.domain;
 
+import com.arquitetura_limpa.Atividade.core.domain.ValueObjects.Idade;
+
+import java.util.UUID;
+
 public class Cliente {
 
+    private UUID id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Float rendaMensal;
+    private Idade idade;
+    private String profissao;
+
+    public Cliente(UUID id, String nome, String email, String telefone, String cpf, Float rendaMensal, Idade idade, String profissao) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.rendaMensal = rendaMensal;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
 }
